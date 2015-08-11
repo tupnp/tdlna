88cd136cf3bac51567273481d28c501180198d3e
Taehyeong <ttaengz@naver.com>
2015-08-12 01:50:48 +0900

    alert 제거 (중간발표)

---
+++ MasterDetailApp/js/main.js
@@ -0,0 +1,28 @@
+//Initialize function
+var init = function () {
+	// TODO:: Do your initialization job
+	console.log("init() called");
+
+	// add eventListener for tizenhwkey
+	document.addEventListener('tizenhwkey', function(e) {
+		if(e.keyName == "back") {
+			try {
+				tizen.application.getCurrentApplication().exit();
+			} catch (error) {
+				console.error("getCurrentApplication(): " + error.message);
+			}
+		}
+	});
+};
+
+$(document).bind( 'pageinit', init );
+
+
+
+
+function serverClick() {
+	window.location.href = "server.html";
+}
+function settingClick(){
+	window.location.href = "setting.html";
+}
