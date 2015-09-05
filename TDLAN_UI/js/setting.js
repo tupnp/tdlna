d9ed41d40c6f9c74ec6553f79df9f30e60222b36
Son <smg2252@gnu.ac.kr>
2015-09-05 23:55:20 +0900

    폴더 리스트 한번에 묶기 전

--- TDLAN_UI/js/setting.js
+++ TDLAN_UI/js/setting.js
@@ -74,20 +74,6 @@ function postNotification(msg){
 	 
 }
 
-function changeSwitch(state){
-	'use strict';
-	if(state.indexOf("OFF") >= 0){
-		console.log('state: OFF');
-	}else{
-		console.log('state: ON');
-		$('.ui-toggle-switch').trigger('click');
-	}
-}
-
-function removeFolder(){
-	 $('#listParent').empty().listview('refresh');
-}
-
 function addFolderContents(count_v,count_i,count_a){
 	//폴더내 미디어 갯수를 출력한다 비디오/이미지/오디오
 	'use strict';
@@ -103,12 +89,12 @@ function addFolderContents(count_v,count_i,count_a){
 	$('#'+'contentsID'+listIndex).append(str);
 }
 
-function buttonOFF(){
+function uncheckListOFF(){
+	console.log("체크리스트만 남김");
 	var alllist = document.getElementById("listParent");   // Get the <ul> element with id="myList"
-	var listArray = document.getElementsByName("checkFolderLi"), listIndex = 0, listCount = 0, nodelete = 0;
+	var listArray = document.getElementsByName("checkFolderLi"), listIndex = 0;
 	console.log(listArray.length + " : 라인 갯수");
-	listCount = listArray.length;
-	while(1){
+	while(true){
 		console.log(listIndex + " : listIndex");
 		if(listIndex === listArray.length){
 			break;
@@ -122,24 +108,6 @@ function buttonOFF(){
 			alllist.removeChild(alllist.childNodes[listIndex]);	
 		}
 	}
-//	for(listIndex = 0 ; listIndex < listCount ; listIndex++){
-//		console.log('#'+'checkInputbox'+listIndex + "버튼 확인");
-////		if($('#'+'checkInputbox'+listIndex).value == "true"){
-//		if(alllist.childNodes[listIndex].value == "true"){
-//			//체크한것
-////			$('#'+'folderLine'+listIndex).attr("src",".icon.png");
-////			$('#'+'checkInputbox'+listIndex).attr("disabled","true");
-//			nodelete++;
-//			console.log()
-//		}else{
-//			alllist.removeChild(alllist.childNodes[nodelete]);// Remove <ul>'s first child node (index 0)
-//			//체크안한것
-////			$('#'+'folderLine'+listIndex).empty();
-////			$('#'+'checkInputbox'+listIndex).attr("src",".icon.png");
-////			$('#'+'checkInputbox'+listIndex).attr("disabled","true");
-//		}
-//		
-//	}
 }
 
 function addFolder(folder_path){
@@ -212,33 +180,166 @@ function sendCommand(command) {
 }
 
 function checkFolder(){
-	console.log("미디어 폴더목록 조회");
+	 $('#listParent').empty().listview('refresh');//기존 폴더 삭제 
+	console.log("기존 목록 삭제 / 미디어 폴더목록 조회요청");
 	sendCommand('media folder');
+}
+
+function failedStart(){
+	alert("실패!!");
+}
+
+
+function showAlert(message) {
+    'use strict';
+    var alertPopup = $('#alert-popup');
+    alertPopup.find('#message').html(message);
+    alertPopup.popup('open', {positionTo: 'window'});
+}
+
+function launchServiceApp() {
+    'use strict';
+    function onSuccess() {
+        console.log('Service App launched successfully!');
+        console.log('Restart...');
+        start();
+    }
+
+    function onError(err) {
+        console.error('Service Applaunch failed', err);
+        isStarting = false;
+        showAlert('Failed to launch HybridServiceApp!');
+    }
+
+    try {
+        console.log('Launching [' + gServiceAppId + '] ...');
+        tizen.application.launch(gServiceAppId, onSuccess, onError);
+    } catch (exc) {
+        console.error('Exception while launching HybridServiceApp: ' +
+            exc.message);
+        showAlert('Exception while launching HybridServiceApp:<br>' +
+            exc.message);
+    }
+}
+
+
+
+function serverOn(selectObject){
+	var state = selectObject.value;
+	if(state.indexOf('on') >= 0){//전원 On
+		console.log("서버켜");
+		sendCommand('dlna on');	
+	}else{
+		console.log("서버꺼");
+		sendCommand('dlna off');
+		//removeFolder();//폴더목록 삭제
+//		postNotification("T-DLNA OFF");
+//		checkFolder();//폴더 새로 생성
+	}
+		
+}
+function changeName(){
+	//device이름 input 태그 표시
+	var name = $('#diviceName').text();
+	$('#diviceName').text('');
+	$('#parent').append('<input id="diviceNameInput" style="font-size: 25pt; color: black; background-color: rgba(255,0,0,0);border:0px;" type="text" value="'
+			+name+'">');
+	$('#diviceNameInput').focus();
+	$('#diviceNameInput').val($('#diviceNameInput').val());
+	$('#settingFooter').show();
+}
+function checkState(){
+	console.log("현재 상태 조회");
+	sendCommand('server state');
+}
+
+function checkName(name){
+	  // 특수문자 제거
+	console.log("1:"+name);
+	var val = name,str;
+	var pattern = /[^(a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣)]/gi;
+	if(pattern.test(name)){
+		name = val.replace(pattern,"");
+	}
+	console.log("2:"+name);
+	
+	//변경된 이름 전달
+	if(name.length === 0){
+		sendCommand('getDeviceId|&');//빈값입력시
+	} else {
+		str = 'getDeviceId|' + name;
+		sendCommand(str);
+	}
+}
+
+function btn_ok(){
+	//device이름 input 태그 표시
+	var name = $('#diviceNameInput').val();
+	$('#diviceNameInput').remove();
+    checkName(name);//디바이스 아이디 가져오기
+	$('#settingFooter').css("display","none");
+}
+function btn_cancel(){
+//	alert('취소');
+	$('#diviceNameInput').remove();
+	checkName();
+	$('#settingFooter').css("display","none");
+}
+
+function changeSwitch(state){
+	'use strict';
+	if(state.indexOf("OFF") >= 0){
+		console.log('state: OFF');
+	}else{
+		console.log('state: ON');
+		$('.ui-toggle-switch').trigger('click');
+	}
 }
 
 function onReceive(data) {
     'use strict';
-    var message, i , state;
+    var message, i , state, contents;
     for (i in data) {
-//    	alert('receved:' + data[i].value);
+    	console.log('receved:' + data[i].value);
         if (data.hasOwnProperty(i) && data[i].key === 'server') {
 			message = data[i].value;
-			
-			if (data[i].value.indexOf('dlna') >= 0) {
-				// alert('receved:' + data[i].value);
-			} else if (data[i].value === 'STATE:OFF') {// DLNA : OFF상태
-				console.log('DLNA OFF 상태');
+//			alert('receved:' + data[i].value);
+			/*if (data[i].value.indexOf('dlna') >= 0) {
+//				 alert('receved:' + data[i].value);
+			} else */
+			if (data[i].value === 'STATE:OFF') {// DLNA가 꺼진 상태
+				console.log('STATE OFF 상태');
 				state = 'OFF';
-				changeSwitch(state);
-			} else if (data[i].value === 'STATE:ON') {// DLNA : OFF상태
-				console.log('DLNA ON 상태');
+				sendCommand('getDeviceId|');//디바이스 아이디 가져오기
+			    checkFolder();
+			} else if (data[i].value === 'STATE:ON') {//  DLNA가 ON 상태
+				console.log('STATE ON 상태');
 				state = 'ON';
 				changeSwitch(state);
-			} else if (data[i].value.indexOf('tDlnaName/') >= 0) {
+				sendCommand('getDeviceId|');
+//				checkFolder();
+//				buttonOFF();
+//				postNotification("T-DLNA ON");
+			}else if (data[i].value === 'DLNA:ON') {// DLNA : ON 상태로 상태 바뀜
+				console.log('DLNA ON 성공');
+				postNotification("T-DLNA ON");
+				uncheckListOFF();
+			}else if (data[i].value === 'DLNA:OFF') {// DLNA : OFF 로 상태 바뀜
+				console.log('DLNA OFF 성공');
+				postNotification("T-DLNA OFF");
+				checkFolder();
+			}else if (data[i].value.indexOf('tDlnaName/') >= 0) {
 				// alert('DLNA NAME:'+data[i].value);
 				var name = data[i].value.split('/');
 				// 이름 바꾸기
 				$('#diviceName').text(name[1]);
+			}
+			else if(data[i].value.indexOf('failed_start') >= 0){
+				failedStart();
+			}else if (data[i].value === 'DLNA:RUNNING') {// 이전부터 실행중
+				console.log('이미 실행중');
+				checkFolder();
+				uncheckListOFF();
 			}
 		}
         else if (data.hasOwnProperty(i) && data[i].key === 'folder_path') {
@@ -248,10 +349,10 @@ function onReceive(data) {
 				addFolder(message);
 			}
 		}
-        else if (data.hasOwnProperty(i) && data[i].key === 'folder_contents') {
-			var contents = data[i].value.split('|');
-			//addFolderContents(contents[0], contents[1], contents[2]); -- 폴더별 컨텐츠 갯수 (사용안함)
-		}
+//        else if (data.hasOwnProperty(i) && data[i].key === 'folder_contents') {
+//			contents = data[i].value.split('|');
+//			//addFolderContents(contents[0], contents[1], contents[2]); -- 폴더별 컨텐츠 갯수 (사용안함)
+//		}
         else{
         	alert('hybrid ERROR! : Please retry');
         }
@@ -286,38 +387,6 @@ function startMessagePort() {
     sendCommand('connect');
 }
 
-function showAlert(message) {
-    'use strict';
-    var alertPopup = $('#alert-popup');
-    alertPopup.find('#message').html(message);
-    alertPopup.popup('open', {positionTo: 'window'});
-}
-
-function launchServiceApp() {
-    'use strict';
-    function onSuccess() {
-        console.log('Service App launched successfully!');
-        console.log('Restart...');
-        start();
-    }
-
-    function onError(err) {
-        console.error('Service Applaunch failed', err);
-        isStarting = false;
-        showAlert('Failed to launch HybridServiceApp!');
-    }
-
-    try {
-        console.log('Launching [' + gServiceAppId + '] ...');
-        tizen.application.launch(gServiceAppId, onSuccess, onError);
-    } catch (exc) {
-        console.error('Exception while launching HybridServiceApp: ' +
-            exc.message);
-        showAlert('Exception while launching HybridServiceApp:<br>' +
-            exc.message);
-    }
-}
-
 function onGetAppsContextSuccess(contexts) {
     'use strict';
     var i, appInfo;
@@ -344,8 +413,8 @@ function onGetAppsContextSuccess(contexts) {
         startMessagePort();
     }
     checkState();//현재 on/off 상태 가져오기
-    sendCommand('getDeviceId|');//디바이스 아이디 가져오기
-    checkFolder();
+//    sendCommand('getDeviceId|');//디바이스 아이디 가져오기
+//    checkFolder();
 }
 
 function onGetAppsContextError(err) {
@@ -392,82 +461,6 @@ function listInstalledApps() {
     } catch (exc) {
         writeToScreen('Get Installed App Info Error');
     }
-}
-
-function serverOn(selectObject){
-//	alert(selectObject.value);
-	var state = selectObject.value;
-	if(state.indexOf('on') >= 0){//전원 On
-		console.log("서버켜");
-		sendCommand('dlna on');	
-	    //checkFolder();//미디어 폴더 가져오기 -- 서버 켜기부터 가져오는걸로 변경
-		postNotification("T-DLNA ON");
-		buttonOFF();
-	}else{
-		console.log("서버꺼");
-		sendCommand('dlna off');
-		//removeFolder();//폴더목록 삭제
-		postNotification("T-DLNA OFF");
-		removeFolder();//기존의 리스트 삭제
-		checkFolder();//폴더 새로 생성
-	}
-		
-}
-function changeName(){
-	//device이름 input 태그 표시
-	var name = $('#diviceName').text();
-	$('#diviceName').text('');
-	$('#parent').append('<input id="diviceNameInput" style="font-size: 25pt; color: black; background-color: rgba(255,0,0,0);border:0px;" type="text" value="'
-			+name+'">');
-	$('#diviceNameInput').focus();
-	$('#diviceNameInput').val($('#diviceNameInput').val());
-	$('#settingFooter').show();
-}
-function checkState(){
-	console.log("현재 상태 조회");
-	sendCommand('server state');
-}
-//function specialCharRemove(obj) {
-//	var val = obj.value;
-////	var pattern = /[^(가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9)]/gi;   // 특수문자 제거
-//	var pattern = /[^(a-zA-Z0-9)]/gi;   // 특수문자 제거
-//	 
-//	//var pattern = /[^(0-9)]/gi;   // 숫자이외는 제거
-//	if(pattern.test(val)){
-//	obj.value = val.replace(pattern,"");
-//	}
-//}
-function checkName(name){
-	  // 특수문자 제거
-	console.log("1:"+name);
-	var val = name;
-	var pattern = /[^(a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣)]/gi;
-	if(pattern.test(name)){
-		name = val.replace(pattern,"");
-	}
-	console.log("2:"+name);
-	
-	//변경된 이름 전달
-	if(name.length == 0){
-		sendCommand('getDeviceId|&');//빈값입력시
-	} else {
-		var str = 'getDeviceId|' + name;
-		sendCommand(str);
-	}
-}
-
-function btn_ok(){
-	//device이름 input 태그 표시
-	var name = $('#diviceNameInput').val();
-	$('#diviceNameInput').remove();
-    checkName(name);//디바이스 아이디 가져오기
-	$('#settingFooter').css("display","none");
-}
-function btn_cancel(){
-//	alert('취소');
-	$('#diviceNameInput').remove();
-	checkName();
-	$('#settingFooter').css("display","none");
 }
 
 $(document).delegate('#setting', 'pageinit', function onMainPageInit() {
